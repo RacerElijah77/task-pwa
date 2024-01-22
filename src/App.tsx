d0486ace772c7857c386a2153c564283cc0bc4d9
Elijah Gasataya <gasatae1@tcnj.edu>
2024-01-22 15:07:09 -0500

    Added intro screen for PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
 import LaunchScreen from './pages/LaunchScreen';
 
 /* Core CSS required for Ionic components to work properly */
@@ -10,7 +11,6 @@ import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
 /* Theme variables */
 import './theme/variables.css';
 import './theme/tailwind.css';
--- src/pages/LaunchScreen.tsx
+++ src/pages/LaunchScreen.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonContent,
   IonHeader,
   IonImg,
@@ -9,15 +10,23 @@ import {
 } from "@ionic/react";
 import "./LaunchScreen.css";
 
-const LaunchScreen: React.FC = () =>{
-    return (
-          <div className= "h-screen flex items-center justify-center">
-            <IonImg></IonImg>
-            <IonLabel className="text-3xl text-center">
-                Welcome to the TASK Learning Module App!
-            </IonLabel>
-          </div>
-    );
+const LaunchScreen: React.FC = () => {
+
+  function handleClick(){
+    alert("You Clicked Me!");
+  }
+
+  return (
+    <IonPage>
+        <div className="h-screen flex gap-y-10 flex-col items-center justify-center bg-cover bg-gradient-to-r from-orange-500 to-yellow-300">
+        <IonImg src="images/task_logo_intro.png" alt="TASK Logo"></IonImg>
+        <IonLabel className="text-3xl text-center">
+            Welcome to the TASK Learning Module App!
+        </IonLabel>
+        <IonButton shape="round" color="secondary" onClick={handleClick}>Next</IonButton>
+        </div>
+    </IonPage>
+  );
 };
 
 export default LaunchScreen;
