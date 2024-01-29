310c1b46c7e5462b28a95c5e21c8647c94ed0fe9
Elijah Gasataya <gasatae1@tcnj.edu>
2024-01-29 12:11:18 -0500

    Removed unwanted CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-import LaunchScreen from './pages/LaunchScreen';
-import AboutAppIntro from './pages/AboutAppIntro';
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+
+import Home from "./pages/Home";
+import LaunchScreen from "./pages/LaunchScreen";
+import AboutAppIntro from "./pages/AboutAppIntro";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 /* Theme variables */
-import './theme/variables.css';
-import './theme/tailwind.css';
+import "./theme/variables.css";
+import "./theme/tailwind.css";
 
 setupIonicReact();
 
--- src/pages/SideBar.tsx
+++ src/pages/SideBar.tsx
@@ -1,13 +1,23 @@
-// Sidepage menu when the user clicks the hamburger menu
+// Sidepage menu component when the user clicks the hamburger menu
 import {
-  IonButtons,
-  IonContent,
+  IonLabel,
   IonFooter,
   IonHeader,
   IonMenu,
   IonTitle,
   IonToolbar,
+  IonItem,
 } from "@ionic/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// Font Awesome imports for sidpage menu icons
+import { faPersonChalkboard } from "@fortawesome/free-solid-svg-icons/faPersonChalkboard";
+import { faGear } from "@fortawesome/free-solid-svg-icons/faGear";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons/faCircleInfo";
+import { faCertificate } from "@fortawesome/free-solid-svg-icons/faCertificate";
+import { fa1 } from "@fortawesome/free-solid-svg-icons/fa1";
+import { fa2 } from "@fortawesome/free-solid-svg-icons/fa2";
+
 import "./SideBar.css";
 
 const SideBar: React.FC = () => {
@@ -15,16 +25,38 @@ const SideBar: React.FC = () => {
     <IonMenu contentId="main-home-content">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>TASK Menu Content</IonTitle>
+          <IonTitle>TASK Menu</IonTitle>
         </IonToolbar>
-        {/* Implement later */}
-        <IonContent>Lesson Modules</IonContent>
-        <IonContent>Misc. Section 1</IonContent>
-        <IonContent>Misc. Section 2</IonContent>
-        <IonContent>Progress</IonContent>
-        <IonContent>Settings</IonContent>
+        <IonItem>
+          <FontAwesomeIcon icon={faPersonChalkboard} className="mr-2" />
+          <IonLabel>Lesson Modules</IonLabel>
+        </IonItem>
+        <IonItem>
+          <FontAwesomeIcon icon={faCertificate} className="mr-2.5" />
+          <IonLabel>Certificate/Progress</IonLabel>
+        </IonItem>
+        <IonItem>
+          <FontAwesomeIcon icon={fa1} className="mr-4" />
+          <IonLabel>Option 1</IonLabel>
+        </IonItem>
+        <IonItem>
+          <FontAwesomeIcon icon={fa2} className="mr-3.5" />
+          <IonLabel>Option 2</IonLabel>
+        </IonItem>
+        <IonItem>
+          <FontAwesomeIcon icon={faGear} className="mr-2" />
+          <IonLabel>Settings</IonLabel>
+        </IonItem>
       </IonHeader>
-      <IonFooter>About TASK</IonFooter>
+
+      <IonFooter>
+        <IonToolbar>
+          <IonTitle>
+            <FontAwesomeIcon icon={faCircleInfo} className="pr-2" />
+            About TASK
+          </IonTitle>
+        </IonToolbar>
+      </IonFooter>
     </IonMenu>
   );
 };
